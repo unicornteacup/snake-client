3b05adebec7aff1bc9fd9c6879af10a3c96e919f
Caroline <carolinejepps@gmail.com>
2020-07-01 20:47:19 +0000

    Added initials

--- client.js
+++ client.js
@@ -8,6 +8,14 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   
+  conn.on('connect', () => {
+    console.log('We\'re connected!');
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: CEJ');
+  });
+
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
