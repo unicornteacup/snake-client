4f28bd23b3b6b217f1ed976148a9033825a2c456
Caroline <carolinejepps@gmail.com>
2020-07-01 22:17:09 +0000

    Added ctrl c exit to play

--- play.js
+++ play.js
@@ -3,3 +3,30 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+
+ const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput)
+
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
+
+
