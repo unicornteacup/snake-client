94d8d0b7dc36c406fb20e39923a8397490448036
Caroline <carolinejepps@gmail.com>
2020-07-01 22:31:38 +0000

    Added input file and updated play to remove input function

---
+++ input.js
@@ -0,0 +1,26 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput)
+
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const inputObject = {
+  setupInput
+}
+
+module.exports = inputObject;
--- play.js
+++ play.js
@@ -1,32 +1,9 @@
 // const net = require('net');
 
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-
-
- const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  stdin.on('data', handleUserInput)
-
-  return stdin;
-}
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
 setupInput();
-
-
